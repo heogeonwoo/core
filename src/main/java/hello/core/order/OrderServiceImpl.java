96f6768d3d5cbb7fe2f9886475e64d97fb56f65b
dev.geonwoo_ <woo6629058@catholic.ac.kr>
2025-08-25 23:29:52 +0900

    refactor: 관심사의 분리 (서비스 구현체 의존성 주입 방식 변경)
    - MemberServiceImpl, OrderServiceImpl을 생성자 주입 방식으로 변경
    - DIP 위반 코드 제거 (구현체 직접 생성 제거)
    - 테스트 코드 수정 후 정상 동작 확인

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,9 +9,20 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    /**
+     * DIP 위반
+     * private final MemberRepository memberRepository = new MemoryMemberRepository();
+     * private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+     * private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+     */
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
