efc1ab09b64fc2f25822ed254900efcbfe6d3134
dev.geonwoo_ <woo6629058@catholic.ac.kr>
2025-08-26 16:05:29 +0900

    refactor: 스프링으로 전환 (스프링 컨테이너 적용)
    - AppConfig를 스프링 설정 클래스(@Configuration, @Bean)로 변환
    - MemberApp, OrderApp을 스프링 컨테이너 기반 코드로 수정
    - 서비스 및 의존성 주입 과정을 스프링 컨테이너가 관리하도록 변경

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,15 +9,15 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
-
     /**
      * DIP 위반
      * private final MemberRepository memberRepository = new MemoryMemberRepository();
      * private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
      * private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
      */
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
